b5dde122c7fafdd5eeb89bbd319b7dac94455d19
GitHub Action <action@github.com>
2023-04-18 07:39:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/gabeiscool420/pycharmgpt/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gabeiscool420/pycharmgpt/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.gabeiscool420.pycharmgpt.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gabeiscool420.pycharmgpt.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
